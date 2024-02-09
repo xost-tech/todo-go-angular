3873769eef33b757f3461ab3be55dc39c990eee9
xost-tech <xost.technologies@gmail.com>
2024-02-09 22:06:22 +0800

    Add Dockerfile for server and update database init script

--- database/1_init.sql
+++ database/1_init.sql
@@ -1,3 +1,8 @@
 create database tododb;
-grant all privileges on tododb.* to 'tododbuser'@'%' identified by 'GHcn=zKhNr5o';
+CREATE USER 'tododbuser'@'%' IDENTIFIED BY 'GHcn=zKhNr5o';
+GRANT ALL PRIVILEGES ON tododb.* TO 'tododbuser'@'%';
+/*grant all privileges on tododb.* to 'tododbuser'@'%' identified by 'GHcn=zKhNr5o';*/
 flush privileges;
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+SET GLOBAL log_bin_trust_function_creators = 0;
